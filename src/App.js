8157e7c40af2bc20692de66b6489204af2453483
fatte7g <fattepuri@outlook.com>
2025-07-11 12:55:51 +0545

    change

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navb from './Navb'
+import Card1 from './Card1'
+
+
+
+
+const App = () => {
+  return (
+    <div>
+       <Navb/>
+      <Card1/>
+    </div>
+  )
+}
+
+export default App
